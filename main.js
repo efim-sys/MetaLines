7c94de1a0fe80b50949f1df506b88a2201dbfbd0
efim-sys <81066363+efim-sys@users.noreply.github.com>
2024-01-25 23:01:48 +0300

    Totaly new 2.0 version
    Optimized code, beautiful ball routes

--- main.js
+++ main.js
@@ -1,183 +1,171 @@
 let balls = ["🔴", "🟡", "🟢", "🔵", "🟣", "⚪"]
 
-let placeholder = "ㅤ"
+let placeholder = " "
 
-let pair = undefined
+const fieldSize = 9
 
-let count = 0
+let area = Array(fieldSize).fill().map(() => Array(fieldSize))
 
-let winCondition = 3
+let activeBall = undefined
 
-let fieldSize = 9
+const winCondition = 5
 
-let cells = new Array(fieldSize*fieldSize)
-
-for (let i = 0; i < fieldSize*fieldSize; i++) {
-    cells[i] = $("<div>", {
-        class: "cell",
-        html: $("<div>", {
-            class: "ball",
-            text: placeholder
-        })
-    }).appendTo("#table-container")
-    cells[i].click( function() {
-        doChange($(this).index())
-    })
-}
-
-async function doChange(item) {
-    
-    if(pair == undefined) {
-        pair = item
-        cells[pair].children().first().css("transform", "scale(1.25, 1.25)")
-    }
-    else if(pair !== item && cells[pair].children().first().text()!==placeholder){
-        if(await findPath(cells[pair], cells[item])) {
-            spawnRandom3()
-            checkLine()
-        }
-        else alert("Ход Заблокирован!")
-        cells[pair].children().first().css("transform", "")
-        pair = undefined
-    }
-    else {
-        cells[pair].children().first().css("transform", "")
-        pair = undefined
-    }
-}
-
-function setRandom(color) {
-    let empty = cells.filter(e => e.children().first().text() === placeholder)
-    if(empty.length === 1) alert("Game Over!")
-    empty[Math.floor(Math.random()*empty.length)].children().first().text(balls[color])
-}
-
-let visited = new Array(fieldSize*fieldSize).fill(false)
-
-let path = []
-
-function checkEmpty(current) {
-    if(current === undefined) return false
-    if(visited[current.index()] === true || current.children().first().text() != placeholder) {
-        visited[current.index()] = true
-        return false
-    }
-    visited[current.index()] = true
-    return true    
-}
-
-function goPath(from, to) {
-    if(from === to) return true
-    if(Math.abs((from.index() % fieldSize) - ((from.index()+1) % fieldSize)) != fieldSize-1 && checkEmpty(cells[from.index()+1])) {
-        if(goPath(cells[from.index()+1], to)) {
-            path.push(from.index())
-            return true
-        }
-    }
-    if(Math.abs((from.index() % fieldSize) - ((from.index()-1) % fieldSize)) != fieldSize-1 && checkEmpty(cells[from.index()-1])) {
-        if(goPath(cells[from.index()-1], to)) {
-            path.push(from.index())
-            return true}
-    }
-    if(checkEmpty(cells[from.index()-fieldSize])) {
-        if(goPath(cells[from.index()-fieldSize], to)) {
-            path.push(from.index())
-            return true}
-    }
-    if(checkEmpty(cells[from.index()+fieldSize])) {
-        if(goPath(cells[from.index()+fieldSize], to)) {
-            path.push(from.index())
-            return true}
-    }
-    return false
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
-
-async function animation() {
-    let color = cells[path[0]].children().first().text()
-    // console.log(color)
-    // console.log(path)
-    for(let i = 0; i < path.length-1; i ++) {
-        cells[path[i+1]].children().first().text(cells[path[i]].children().first().text())
-        cells[path[i]].children().first().text(placeholder)
-        await sleep(30)
-    }
-}
-
-async function findPath(from, to) {
-    path = []
-    visited.fill(false)
-    let res = goPath(from, to)
-    path.reverse()
-    path.push(to.index())
-    if(res) {
-        await animation()        
-    }
-    return res
-}
-
-async function spawnRandom3() {
-    for(let i = 0; i < 3; i++) {
-        setRandom(Math.floor(Math.random()*balls.length))
-        // setRandom(2)
-    }
-}
-
-async function checkLine() {
-    removalMatrix = new Array(fieldSize*fieldSize).fill(" ")
-    matrix = []
-    for(let i = 0; i < fieldSize; i++) {
-        s = ""
-        for(let j = 0; j < fieldSize; j++) {
-            s+=cells[i*fieldSize+j].children().first().text()
-        }
-        matrix.push(s)
-    }
-    
-    matrix.forEach(function(row, ind) {
-        finding = row
-        for(let nm = 0; nm < balls.length; nm++) {
-            for(let ln = fieldSize; ln >= winCondition; ln--) {
-                finding = finding.replace(balls[nm].repeat(ln), "*".repeat(ln))
+for (let y = 0; y < fieldSize; y++) {
+    for (let x = 0; x < fieldSize; x++) {
+        area[x][y] = $("<div>", {
+            class: "cell",
+            html: $("<div>", {
+                class: "ball",
+                text: placeholder
+            })
+        }).appendTo("#table-container")
+        area[x][y].click( function() {
+            console.log(`X=${x}; Y=${y}`)
+            thisBall = {x: x, y: y}
+            if (activeBall == undefined) {
+                activeBall = thisBall
+                area[activeBall.x][activeBall.y].children().first().css("transform", "scale(1.25, 1.25)")
             }
-        }
-        ([...finding]).forEach(function(e, ind2) { 
-            if (e == "*") removalMatrix[ind*fieldSize+ind2] = "*" 
-            
+            else {
+                if((activeBall.x != thisBall.x) || (activeBall.y != thisBall.y)) {
+                    let result = move(activeBall, thisBall)
+                    if(result == false) alert("Ход невозможен!")
+                    else {
+                        animate(result)
+                        setTimeout(() => {
+                            if(!checkLine()) spawnRandomBalls(3)
+                        }, 50*(result.length+1))
+                    }
+                }
+                area[activeBall.x][activeBall.y].children().first().css("transform", "")
+                activeBall = undefined
+            }
         })
-    })
-    matrix = []
-    for(let i = 0; i < fieldSize; i++) {
-        s = ""
-        for(let j = 0; j < fieldSize; j++) {
-            s+=cells[j*fieldSize+i].children().first().text()
-        }
-        matrix.push(s)
     }
+}
+
+let currentStep = 0
+let globalPath  = []
+
+let start = null
+
+function animationFrame(timestamp) {
+    if(!start) start = timestamp
+    if(timestamp-start > 50) {
+        switchCells(globalPath[currentStep], globalPath[currentStep+1])
+        start = timestamp
+        currentStep+=1
+        console.log(globalPath[currentStep])
+    }
+    if(currentStep<globalPath.length-1) window.requestAnimationFrame(animationFrame)
+}
+
+function animate(path) {
+    console.log(path)
+    globalPath  = path
+    currentStep = 0
+    window.requestAnimationFrame(animationFrame)
+} 
+
+function switchCells(a, b) {
+    setText(b, getText(a))
+    setText(a, placeholder)
+}
+
+function getText(coordinates) {
+    return area[coordinates.x][coordinates.y].children().first().text()
+}
+
+function setText(coordinates, value) {
+    return area[coordinates.x][coordinates.y].children().first().text(value)
+}
+
+function isCellFree(x, y) {
+    return getText({x: x, y: y}) == placeholder
+}
+
+function getFreeCells() {
+    let freeCells = []
+    for (let x = 0; x < fieldSize; x++) {
+        for (let y = 0; y < fieldSize; y++) {
+            if(isCellFree(x, y)) freeCells.push({x: x, y: y})
+        }
+    }
+    return freeCells
+}
+
+function move(start, to) {
+    from = new Array(fieldSize).fill().map(() => new Array(fieldSize).fill(-1))
+    distances = new Array(fieldSize).fill().map(() => new Array(fieldSize).fill(Infinity))
     
-    matrix.forEach(function(row, ind) {
+    queue = []
+    queue.push(start)
+    distances[start.x][start.y] = 0
+    
+    while (queue.length > 0) {
+        let pos = queue.shift()
         
-        finding = row
-        // console.log(finding)
-        for(let nm = 0; nm < balls.length; nm++) {
-            for(let ln = fieldSize; ln >= winCondition; ln--) {
-                finding = finding.replace(balls[nm].repeat(ln), "*".repeat(ln))
+        let deltas = [{dx: 0, dy: 1}, {dx: 0, dy: -1}, {dx: -1, dy: 0}, {dx: 1, dy: 0}]
+        deltas.forEach(delta => {            // Смещения
+            let cell = {x: pos.x+delta.dx, y: pos.y+delta.dy}
+            if(cell.x>=0 && cell.x<=8 && cell.y>=0 && cell.y<=8 && distances[cell.x][cell.y]==Infinity && isCellFree(cell.x, cell.y)) {  // Полученная клетка в границах поля
+                distances[cell.x][cell.y] = distances[pos.x][pos.y]+1
+                queue.push(cell)
+                from[cell.x][cell.y] = pos
             }
-        }
-        ([...finding]).forEach(function(e, ind2) { 
-            if (e == "*") removalMatrix[ind2*fieldSize+ind] = "*" 
-            
         })
-    })
-    // console.log(removalMatrix)
-    removalMatrix.forEach((e,ind) => {
-        if(e == "*") cells[ind].children().first().text(placeholder)
-    })
-    
+    }
 
+    if (distances[to.x][to.y] == Infinity) return false
+
+    path = []
+    for (let pos = to; pos != -1; pos = from[pos.x][pos.y]) {
+        path.unshift(pos)
+    }
+    return path
 }
 
-spawnRandom3()
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function randomBall() {
+    return balls[randomNumber(0, balls.length)]
+}
+
+function spawnRandomBalls(amount) {
+    let available = getFreeCells().sort(()=>Math.random()-0.5).slice(-amount)
+    if(available.length == 0) return false
+    available.forEach(e => {setText(e, randomBall())})
+    return true
+}
+
+function checkLine() {
+    hStrings = new Array(fieldSize).fill("")
+    vStrings = new Array(fieldSize).fill("")
+    for (let y = 0; y < fieldSize; y++) {
+        for (let x = 0; x < fieldSize; x++) {
+            hStrings[y]+=getText({x:x,y:y})
+            vStrings[x]+=getText({x:x,y:y})
+    }}
+    for(let row = 0; row < fieldSize; row++) {
+        balls.forEach(color => {
+            for(let len = fieldSize; len >= winCondition; len --) {
+                hStrings[row] = hStrings[row].replace(color.repeat(len), "*".repeat(len))
+                vStrings[row] = vStrings[row].replace(color.repeat(len), "*".repeat(len))
+            }
+        })
+    }
+    for(let row = 0; row < fieldSize; row++) {
+        hStrings[row] = [...hStrings[row]]
+        vStrings[row] = [...vStrings[row]]
+    }
+    for (let y = 0; y < fieldSize; y++) {
+        for (let x = 0; x < fieldSize; x++) {
+            if(hStrings[y][x]=="*") setText({x:x,y:y}, placeholder)
+            if(vStrings[y][x]=="*") setText({x:y,y:x}, placeholder)
+    }}
+    return hStrings.concat(vStrings).flat(Infinity).indexOf("*")>0
+}
+
+spawnRandomBalls(3)
